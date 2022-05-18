47fd8a4e3f47a5ecce1e75ed6de809ceb1e66dfc
D <63995516+dDee-D@users.noreply.github.com>
2022-05-18 12:04:01 +0700

    init

--- dee-juno-cosmwasm/src/contract.rs
+++ dee-juno-cosmwasm/src/contract.rs
@@ -1,15 +1,15 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{to_binary,Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, StdError,Uint64};
-use std::ops::Add;
-use cosmwasm_std::attr;
+use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+use cw2::set_contract_version;
+
 use crate::error::ContractError;
-use crate::msg::{ ExecuteMsg, InstantiateMsg, QueryMsg};
-use crate::state::{DEE_COUNT};
+use crate::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{State, STATE};
 
 // version info for migration info
-// const CONTRACT_NAME: &str = "crates.io:dee-cosm";
-// const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+const CONTRACT_NAME: &str = "crates.io:dee-juno-cosmwasm";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -18,87 +18,87 @@ pub fn instantiate(
     info: MessageInfo,
     msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    DEE_COUNT.save(deps.storage, &Uint64::zero())?;
-    let res = Response::new();
-    let _ = info;
-    let _ = msg;
-    Ok(res)
+    let state = State {
+        count: msg.count,
+        owner: info.sender.clone(),
+    };
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    STATE.save(deps.storage, &state)?;
+
+    Ok(Response::new()
+        .add_attribute("method", "instantiate")
+        .add_attribute("owner", info.sender)
+        .add_attribute("count", msg.count.to_string()))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
     deps: DepsMut,
     _env: Env,
-    // info: MessageInfo,
+    info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    let mut count = Uint64::zero();
-    match msg{
-        ExecuteMsg:: Dee{} =>{
-           let _:Result<Uint64,StdError> = DEE_COUNT.update(deps.storage, |exists|{
-                count = exists.add(Uint64::from(1u8));
-                Ok(count)
-            });
-          let mut res = Response::new();
-          res.attributes.push(attr("dee_count",count));
-          res.attributes.push(attr("dee_count","haha"));
-          res.data = Some(to_binary(&count)?);
-          Ok(res)
-        }
+    match msg {
+        ExecuteMsg::Increment {} => try_increment(deps),
+        ExecuteMsg::Reset { count } => try_reset(deps, info, count),
     }
 }
 
+pub fn try_increment(deps: DepsMut) -> Result<Response, ContractError> {
+    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
+        state.count += 1;
+        Ok(state)
+    })?;
 
-#[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
-    let count:Uint64 = DEE_COUNT.load(deps.storage)?;
-    to_binary(&count)
+    Ok(Response::new().add_attribute("method", "try_increment"))
 }
 
+pub fn try_reset(deps: DepsMut, info: MessageInfo, count: i32) -> Result<Response, ContractError> {
+    STATE.update(deps.storage, |mut state| -> Result<_, ContractError> {
+        if info.sender != state.owner {
+            return Err(ContractError::Unauthorized {});
+        }
+        state.count = count;
+        Ok(state)
+    })?;
+    Ok(Response::new().add_attribute("method", "reset"))
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
+    }
+}
+
+fn query_count(deps: Deps) -> StdResult<CountResponse> {
+    let state = STATE.load(deps.storage)?;
+    Ok(CountResponse { count: state.count })
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use cosmwasm_std::testing::{mock_dependencies_with_balance, mock_env, mock_info, mock_dependencies};
+    use cosmwasm_std::testing::{mock_dependencies_with_balance, mock_env, mock_info};
     use cosmwasm_std::{coins, from_binary};
 
-    
     #[test]
     fn proper_initialization() {
         let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
 
-        let msg = InstantiateMsg {};
-        let info = mock_info("creator",&[]);
+        let msg = InstantiateMsg { count: 17 };
+        let info = mock_info("creator", &coins(1000, "earth"));
 
-        instantiate(deps.as_mut(),mock_env(), info.clone(),msg.clone()).unwrap();
- 
         // we can just call .unwrap() to assert this was a success
-        let msg = ExecuteMsg::Dee{};
-        let res = execute(deps.as_mut(),mock_env(), msg.clone()).unwrap();
+        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
         assert_eq!(0, res.messages.len());
 
         // it worked, let's query the state
         let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: Uint64 = from_binary(&res).unwrap();
-        assert_eq!(Uint64::zero(), value);
+        let value: CountResponse = from_binary(&res).unwrap();
+        assert_eq!(17, value.count);
     }
 
-    #[test]
-    fn test_dee(){
-        let mut deps = mock_dependencies();
-
-        let msg = InstantiateMsg{};
-        let info = mock_info("creator",&[]);
-
-        instantiate(deps.as_mut(),mock_env(),info.clone(),msg.clone()).unwrap();
-        
-        let msg = ExecuteMsg::Dee{};
-        let res = (deps.as_mut(),mock_env(),info.clone(),msg.clone()).unwrap();
-        assert_eq!(res.attributes.len(),1);
-        assert_eq!(res.attributes. vec![attr(dee_count, 1)]);
-        let data:String = from_binary(value: &Binary)
-    }
-    /*
     #[test]
     fn increment() {
         let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
@@ -117,9 +117,7 @@ mod tests {
         let value: CountResponse = from_binary(&res).unwrap();
         assert_eq!(18, value.count);
     }
-    */
 
-/*
     #[test]
     fn reset() {
         let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
@@ -147,5 +145,4 @@ mod tests {
         let value: CountResponse = from_binary(&res).unwrap();
         assert_eq!(5, value.count);
     }
-*/
 }
